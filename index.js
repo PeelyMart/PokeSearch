531858c770ff7ced18cebe53e41edbf791e518ce
PeelyMart <philipmartinantolihao@gmail.com>
2026-01-10 00:58:39 +0800

    UPLOAD: REDIRECT + DATA MGMT
    API connection is coded in: Works (we get a response)
    
    API Connection: Unkown if sessionStorage is usable
    
    Blank placeholder: html for landing page of query in progress

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-document.getElementById("search").addEventListener("click", function(){
+document.getElementById("search").addEventListener("click", async function(){
 	const pokemon = document.getElementById("pokemon").value;
 
-	if(pokemon !== ""){
-		console.log(pokemon);
-
-	}
-	else{
+	if(!pokemon){
 		document.getElementById("errMsg").textContent = "please input something!";
 	}
 
+	const data = await fetchData(pokemon);
+
+	if(data != null){
+		sessionStorage.setItem("pokeData", JSON.stringify(data));
+		window.location.href = "results.html";
+
+
+	}
+	else{
+		
+	document.getElementById("errMsg").textContent = "Pokemon not found!";
+
+	}
 
 
 })
+
+
+
+async function fetchData(query){
+
+	try{
+		const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query.toLowerCase()}`);
+			if(!response.ok){
+				throw new Error("Pokemon not found");
+
+
+			}
+		const data = await response.json();
+		return data;
+	}catch(error){
+		console.error(error);
+		return null;
+	}
+
+}
